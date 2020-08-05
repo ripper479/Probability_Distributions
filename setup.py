ee5654d5c695842bc578d287dd9cddc4f14ffc3f
Sourav Sharma <33860939+ripper479@users.noreply.github.com>
2020-08-05 14:15:16 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distributions_2020_1',
+      version='0.1',
+      description='Gaussian distributions',
+      author='Sourav Sharma' ,
+      author_email = 'sourav.sharma479@gmail.com' ,
+      packages=['probability_distributions_2020_1'],
+      zip_safe=False)
